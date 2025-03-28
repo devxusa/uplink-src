aa391016fa295d8bb133f4fdbc1e3b43639ba815
devx <hauntgt@gmail.com>
2025-03-27 21:54:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ async def submit(ctx):
             "1. Game Title\n"
             "2. Short Description\n"
             "3. Tags and Categories\n\n"
-            "⚠️WARNING⚠️ We are investigating the issue with the images. Please do NOT upload an image with your game, we will automatically add one."
+            "⚠️WARNING⚠️ We are investigating the issue with the images. Please do NOT upload an image with your game, we will automatically add one.\n"
             "Click the button below to submit your game."
         ),
         color=discord.Color.purple()
